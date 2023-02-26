5680200b1f4dddcf5357ccd72c2fb5c7255e84e4
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-26 18:54:48 +0530

    types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
-let username = "Ashmeet";
-console.log(username);
+let myName: string;
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+myName = "Ash";
+meaningOfLife = 42;
+isLoading = true;
+album = 5150;
+
+const sum = (a: number, b: number) => {
+	return a + b;
+};
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g;
