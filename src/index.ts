c38e89092e530c6a39739720626cb40f6bea1b1f
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-01 22:46:15 +0530

    T generic type interface

--- src/index.ts
+++ src/index.ts
@@ -347,20 +347,20 @@ console.log(isTrue(-0));
 
 // ////////////////////////////////////
 
-// interface BoolCheck<T> {
-//     value: T,
-//     is: boolean,
-// }
+interface BoolCheck<T> {
+	value: T;
+	is: boolean;
+}
 
-// const checkBoolValue = <T>(arg: T): BoolCheck<T> => {
-//     if (Array.isArray(arg) && !arg.length) {
-//         return { value: arg, is: false }
-//     }
-//     if (isObj(arg) && !Object.keys(arg as keyof T).length) {
-//         return { value: arg, is: false }
-//     }
-//     return { value: arg, is: !!arg }
-// }
+const checkBoolValue = <T>(arg: T): BoolCheck<T> => {
+	if (Array.isArray(arg) && !arg.length) {
+		return { value: arg, is: false };
+	}
+	if (isObj(arg) && !Object.keys(arg as keyof T).length) {
+		return { value: arg, is: false };
+	}
+	return { value: arg, is: !!arg };
+};
 
 // //////////////////////////////////////
 
